5363767a7230ca5c422f1982d5e63cd65e0e9a9b
Roberson Garcia <roberson.gar12@gmail.com>
2023-05-15 18:53:53 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+require('colors')
+const {showMenu, pause} = require('./helpers/messages')
+
+console.clear()
+
+const main = async () => {
+
+    let opt = ''
+
+    do{
+        opt = await showMenu()
+        await pause()
+    }while(opt !== '0')
+}
+
+main()
